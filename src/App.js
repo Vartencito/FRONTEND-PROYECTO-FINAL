63ea51c7fd8792d0db17dbe09024ddca5023410a
Valen <vatenenberg@gmail.com>
2022-05-25 19:06:54 -0300

    intentamos conectar la api pero no pudimos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Imagen from './components/Imagen';
+// import Imagen from './components/Imagen';
 import Description from './components/Description';
-// import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
@@ -15,6 +16,15 @@ function App() {
   //lo que se ejecuta siempre
   })*/
   
+let [imagenes, setImagenes] = useState(''); 
+
+useEffect(() =>{
+  async function buscarPublicaciones(){
+    const res = await axios.get("http://localhost:4000/publicaciones/1");
+    setImagenes(res.data);
+    return imagenes;
+  }
+},[])
 
 
 const publication = 
@@ -28,8 +38,21 @@ return (
 <>
 <div className="container-fluid" >
 <div className="text-center-center">
-      <Imagen url={publication.url} />
+      {/* <Imagen url={publication.url} /> */}
+      {/* <img src={imagen} /> */}
       <Description description={publication.description} />
+      <input placeholder='Id usuario'/>
+      <input placeholder='Id publicacion'/>
+      <button>Enviar</button>
+
+      {
+
+          imagenes.map(imagen => (
+            <div>
+                <p>{ imagen.name }</p>
+            </div>
+          ))
+      }
 </div>
 </div>
 </>
